a465973a47b3f6a20b083fdbc96b17169307b9b9
Raghav1598 <raghavkwal68@gmail.com>
2021-12-28 19:32:55 +0530

    LoginPage.js created

--- ecom-frontend/src/App.js
+++ ecom-frontend/src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from 'customs/Header';
-import Footer from 'customs/Footer';
+import Header from './customs/Header';
+import Footer from './customs/Footer';
+import LoginPage from './Signup/LoginPage';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <LoginPage></LoginPage>
       <Footer></Footer>
     </div>
   );
