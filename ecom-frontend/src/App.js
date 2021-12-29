0e681424e73d2e63ac3c0e17046c49145aca2257
Raghav1598 <raghavkwal68@gmail.com>
2021-12-29 20:48:40 +0530

    Modified LoginPage.js

--- ecom-frontend/src/App.js
+++ ecom-frontend/src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import Header from './customs/Header';
 import Footer from './customs/Footer';
 import LoginPage from './Signup/LoginPage';
+//import Login from './Signup/Login';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      {
+        //<Login></Login>
+      }
       <LoginPage></LoginPage>
       <Footer></Footer>
     </div>
--- ecom-frontend/src/Signup/LoginPage.js
+++ ecom-frontend/src/Signup/LoginPage.js
@@ -1,38 +1,43 @@
-//import React, {useState} from "react";
+import React, {useState} from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import validator from 'validator';
+import './LoginPage.css'
 
 function LoginPage() {
 
-    //const[email, setEmail] = useState('');
-    //const[password, setPassword] = useState('');
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
 
+    function validate(){
+      if(validator.isEmail(email)){
+        return password.length>0;
+      }
+      return false;
+    }
 
     return (
-        <div className="login">
-            <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-        </div>
+        <Container className="center">
+          <Row>
+            <Col>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </Form.Group>
+            <Button variant="primary" type="submit" disabled={!validate()}>Submit</Button>
+          </Form>
+          </Col>
+          </Row>
+        </Container>
     );
 }
-
-// function validateForm(){
-//     return true;
-// }
 
 export default LoginPage;
