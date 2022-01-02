12fe9ba2b8c01de65a0f0c986a758560764d9a76
Raghav1598 <raghavkwal68@gmail.com>
2022-01-02 12:09:06 +0530

    Cross Origin Access

--- e-com-dummy/src/main/java/com/project/ecom/controller/LoginController.java
+++ e-com-dummy/src/main/java/com/project/ecom/controller/LoginController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import com.project.ecom.service.LoginService;
 
 @RestController
 @RequestMapping("/signup")
+@CrossOrigin(origins="http://localhost:3000")
 public class LoginController {
 	
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
