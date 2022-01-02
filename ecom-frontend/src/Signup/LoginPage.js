da6e5339bf97553b9ff4b4428bd1d44870480708
Raghav1598 <raghavkwal68@gmail.com>
2022-01-02 12:28:31 +0530

    Changes for Header Component

--- ecom-frontend/src/Signup/LoginPage.js
+++ ecom-frontend/src/Signup/LoginPage.js
@@ -5,6 +5,7 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import validator from 'validator';
+import LoginService from "../services/LoginService";
 import './LoginPage.css'
 
 function LoginPage() {
@@ -14,25 +15,38 @@ function LoginPage() {
 
     function validate(){
       if(validator.isEmail(email)){
-        return password.length>0;
+        return password.length>8;
       }
       return false;
+    }
+
+    function authenticate(){
+      LoginService.loginHandler(email).then(response => handleResponse(response));
+    }
+
+    function handleResponse(response){
+      let expectedPassword = response.data.password;
+      if(expectedPassword === password){
+        
+      }else console.log(password);
     }
 
     return (
         <Container className="center">
           <Row>
             <Col>
-          <Form>
+          <Form onSubmit={(e) => e.preventDefault()}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
               <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+              <Form.Text>*Please enter valid Email-Address to enable Submit Button.*</Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <Form.Text>*The Password Should be eight characters long.*</Form.Text>
             </Form.Group>
-            <Button variant="primary" type="submit" disabled={!validate()}>Submit</Button>
+            <Button variant="primary" type="submit" disabled={!validate()} onClick={authenticate}>Submit</Button>
           </Form>
           </Col>
           </Row>
--- ecom-frontend/src/customs/Header.js
+++ ecom-frontend/src/customs/Header.js
@@ -1,8 +1,22 @@
+import {Link} from 'react-router-dom';
+
 function Header(){
+
+    const isUserLoggedIn = false;
 
     return (
         <header>
-
+            <nav className='navbar navbar-expand-md navbar-dark bg-dark'>
+                EComApp
+                <ul className='navbar-nav'>
+                    {isUserLoggedIn && <li><Link to="/welcome/raghav" className='nav-link'>Home</Link></li>}
+                    {isUserLoggedIn && <li><Link to="/orders" className='nav-link'>Orders</Link></li>}
+                </ul>
+                <ul className='navbar-nav navbar-collapse justify-content-end'>
+                    {!isUserLoggedIn && <li><Link to="/login" className='nav-link'>Login</Link></li>}
+                    {isUserLoggedIn && <li><Link to="/logout" className='nav-link'>Logout</Link></li>}
+                </ul>
+            </nav>
         </header>
     );
 }
